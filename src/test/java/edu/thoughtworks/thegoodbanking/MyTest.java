914a0ab398cb4e7e9cb45e0c057e5635985776c8
Eduard Maura i Puig <edu.maura.puig@gmail.com>
2019-10-22 09:44:55 +0200

    Add test to trigger CI

---
+++ src/test/java/edu/thoughtworks/thegoodbanking/MyTest.java
@@ -0,0 +1,15 @@
+package edu.thoughtworks.thegoodbanking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class MyTest {
+
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+
+}
