7b5f9cdd5ca8f3c246df648788c3294c749b58b7
Eduard Maura i Puig <edu.maura.puig@gmail.com>
2019-11-05 13:15:13 +0100

    Add main webpage and easy error page

--- src/main/java/edu/thoughtworks/thegoodbanking/ApiController.java
+++ src/main/java/edu/thoughtworks/thegoodbanking/ApiController.java
@@ -1,13 +1,24 @@
 package edu.thoughtworks.thegoodbanking;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class ApiController {
-    @GetMapping("/")
+    @GetMapping("/status")
     @ResponseBody
-    public String welcome() {
-        return "Hello World!";
+    public String status() {
+        return "UP!";
+    }
+
+    @RequestMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 }
