5affb9025e4f20cd728c921a57c1a209b82873ce
Eduard Maura i Puig <edu.maura.puig@gmail.com>
2019-11-23 12:24:36 +0100

    Add empty html page on root endpoint to pass test - Add start application on acceptance test - Add mapping between static resources and endpoints

---
+++ src/main/java/edu/thoughtworks/thegoodbanking/WebController.java
@@ -0,0 +1,13 @@
+package edu.thoughtworks.thegoodbanking;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+}
--- src/test-acceptance/java/edu/thoughtworks/thegoodbanking/TransferAcceptanceTest.java
+++ src/test-acceptance/java/edu/thoughtworks/thegoodbanking/TransferAcceptanceTest.java
@@ -3,9 +3,14 @@ package edu.thoughtworks.thegoodbanking;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class TransferAcceptanceTest {
     @Test
     public void shouldNotAllowTransfersToTheSameAccountAsSource() throws IOException {
